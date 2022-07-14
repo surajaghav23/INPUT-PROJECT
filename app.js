a6bd6bce6cadcf064342d4a24b8fc098909011d6
unknown <surajaghav23@gmail.com>
2022-07-14 23:40:56 +0530

    with userAgent api

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var iOS = navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
+var iOS = navigator.userAgent.platform && /iPad|iPhone|iPod/.test(navigator.userAgent.platform);
 if (iOS)
     document.head.querySelector('meta[name="viewport"]').content = "width=device-width, initial-scale=1, maximum-scale=1";
 else
