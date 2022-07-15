da5a3aec7e1fbf3b5adf2df870669c155ea1e07d
unknown <surajaghav23@gmail.com>
2022-07-15 17:21:41 +0530

    regEx comparison

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var iOS = navigator.userAgent.platform && /iPad|iPhone|iPod/.test(navigator.userAgent.platform);
+var iOS = navigator.platform && /iPhone/.test(navigator.platform);
 if (iOS)
     document.head.querySelector('meta[name="viewport"]').content = "width=device-width, initial-scale=1, maximum-scale=1";
 else
