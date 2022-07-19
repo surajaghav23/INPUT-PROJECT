e329477512555122617ae18d843c6638b406935b
unknown <surajaghav23@gmail.com>
2022-07-19 16:16:20 +0530

    Added script for conditional render

--- app.js
+++
@@ -1,5 +0,0 @@
-var iOS = navigator.platform && /iPhone/.test(navigator.platform);
-if (iOS)
-    document.head.querySelector('meta[name="viewport"]').content = "width=device-width, initial-scale=1, maximum-scale=1";
-else
-    document.head.querySelector('meta[name="viewport"]').content = "width=device-width, initial-scale=1";
